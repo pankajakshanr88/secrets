acb1a5e729974236484967f34c03c124f1a3c9fa
pankajakshanr88 <55806937+pankajakshanr88@users.noreply.github.com>
2020-06-28 17:28:17 -0400

    bcrypt

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
 const app = express();
-const encrypt=require("mongoose-encryption")
+const bcrypt = require('bcrypt');
 const port = process.env.PORT || 3000;
-
+const saltRounds = 10;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -21,7 +21,7 @@ const userSchema=new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 const User= new mongoose.model("User",userSchema)
 app.get('/', (req, res) => {
   res.render('home');
@@ -34,15 +34,16 @@ app.route('/login')
 .post(function(req,res){
   const username=req.body.username;
   const password=req.body.password;
-
+console.log(password);
   User.findOne({email:username},function(err,founduser){
     if(err){
       console.log("Incorrect username"+err)
     }else{
-      if(founduser.password===password){
-        res.render("secrets")
-      }else {console.log("Incorrect password"+founduser.password+"is not same as"+password)}
-    }
+      bcrypt.compare(password,founduser.password, function(err, result) {
+        if(result===true){  res.render("secrets")}
+
+      })
+      }
   });
 });
 
@@ -52,19 +53,21 @@ app.route('/register')
   res.render('register');
 })
 .post(function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:req.body.password
-  })
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  })
-
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+      })
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    })
 });
+  });
 
 
 
